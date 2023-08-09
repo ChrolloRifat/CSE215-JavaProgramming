dd9437d4ceebd7f09e351955dcea885c24cd7bc9
ChrolloRif <01914rifat@gmail.com>
2023-08-09 08:06:47 +0600

    Edit loops.java

--- src/Loops.java
+++ src/Loops.java
@@ -3,7 +3,7 @@ public class Loops {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		loop2();
+		loop3();
 	}
 	
 	public static void loop1() {
@@ -15,25 +15,50 @@ public class Loops {
 		}
 	}
 	public static void loop2() {
-		for (int i = 0; i < 3; i++) {
-            for (int j = 2 - i; j > 0; j--) {
+		final int ROWS = 3;
+		int stars = 1;
+		int spaces = ROWS -1;
+		for (int i = 1; i < 2 * ROWS; i++) {
+			
+            for (int j = 1; j <= spaces; j++) {
                 System.out.print(" ");
             }
-            for (int k = 0; k <= 2 * i; k++) {
+            for (int k = 1; k < 2 * stars; k++) {
                 System.out.print("*");
             }
             System.out.println();
-        }
-		for (int i = 1; i > 0; i--) {
-            for (int j = 0; j < 2 - i; j++) {
-                System.out.print(" ");
+            if(i < ROWS) {
+            	spaces --;
+            	stars ++;
+            	
             }
-            for (int k = 2 * i; k >= 0; k--) {
-                System.out.print("*");
+            else {
+            	spaces++;
+            	stars--;
+            	
             }
-            System.out.println();
         }
 		
+		
+	}
+	
+	public static void loop3() {
+		final int Rows = 4;
+		int num = (Rows * 2) - 1;
+		int spaces = 0;
+		for(int i = 1; i <= Rows; i++) {
+			for(int j = 1; j <= spaces; j++) {
+				System.out.print(" ");
+			}
+			for(int k = 1; k <= num ; k++) {
+				System.out.print("1");
+				
+			}
+			
+			System.out.println();
+			spaces++;
+			num -= 2;
+		}
 	}
 	
 
