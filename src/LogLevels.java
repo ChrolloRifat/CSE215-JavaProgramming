b8c9cee9d4e2dab98694a556c05efb793671ad7b
ChrolloRifat <officialmhrifat@outlook.com>
2023-08-19 22:11:22 +0600

    Edit LogLevels

--- src/LogLevels.java
+++ src/LogLevels.java
@@ -5,7 +5,8 @@ public class LogLevels {
 		// TODO Auto-generated method stub
 		
 		System.out.println(LogLevels.message("[ERROR]: Invalid operation"));
-		System.out.println(LogLevels.logLevel("[ERROR]: Invalid operation"));		
+		System.out.println(LogLevels.logLevel("[ERROR]: Invalid operation"));
+		System.out.println(LogLevels.reformat("[ERROR]: Operation Completed"));		
 	}
 
 	public static String message(String logLine) {
@@ -20,5 +21,11 @@ public class LogLevels {
         int start = mes.indexOf("[") + 1;
         int end = mes.indexOf("]"); 
 		 return mes.substring(start,end).toLowerCase().trim();
-    }	
+    }
+	public static String reformat(String logLine) {
+        String s1 = LogLevels.message(logLine);
+        String s2 = LogLevels.logLevel(logLine);
+        return s1 + " (" + s2 + ")";
+       
+    }
 }
