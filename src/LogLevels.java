c5157fdae18a6e5f0042f247eb376a72a108e65a
ChrolloRifat <officialmhrifat@outlook.com>
2023-08-19 21:59:28 +0600

    Edit LogLevel

--- src/LogLevels.java
+++ src/LogLevels.java
@@ -5,15 +5,20 @@ public class LogLevels {
 		// TODO Auto-generated method stub
 		
 		System.out.println(LogLevels.message("[ERROR]: Invalid operation"));
-		
+		System.out.println(LogLevels.logLevel("[ERROR]: Invalid operation"));		
 	}
 
 	public static String message(String logLine) {
     String mes = logLine.trim();
     int start = mes.indexOf(":") + 1;
     int end = mes.length();
-    System.out.println(end);
+    
     return mes.substring(start,end).trim();
 	}
-	
+    public static String logLevel(String logLine) {
+        String mes = logLine.trim();
+        int start = mes.indexOf("[") + 1;
+        int end = mes.indexOf("]"); 
+		 return mes.substring(start,end).toLowerCase().trim();
+    }	
 }
