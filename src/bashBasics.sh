8d1ed31dbc9ba0c77d35099fb3140e3001f9763e
ChrolloRifat <officialmhrifat@outlook.com>
2023-08-20 16:38:20 +0600

    Edit bashBasics

--- src/bashBasics.sh
+++ src/bashBasics.sh
@@ -19,9 +19,39 @@ sub=$(($num1-$num2))					#cannot use space before and after symbols
 echo "The sum of $num1 and $num2 is $sum"
 echo "The subraction of $num1 and $num2 is $sub"
 
+# Doing the type of division where  there are fractions in the quotient
+quotient=$(echo "$num1/$num2" | bc -l)
+echo "The result of $num1/$num2 is : $quotient"
+
 
 
 array=(C Java "C++" JavaScript Ruby Python)	#creates an array and stores the elements enclosed by the ()
 
 echo "${array[@]}"			#prints all the elements of the array. ${array_name[element_index]}  
 echo "Total = ${#array[@]}" #prints the length of the array
+
+
+
+##Working with strings
+read -p "Enter a string: " str
+echo "Length of the string is ${#str}"
+read -p "Enter another string: " new_str
+joined_str=$str$new_str
+echo "Joined string: $joined_str"
+substring=${str:$1:$4}
+echo "A substring extracted from earlier string: $substring"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
