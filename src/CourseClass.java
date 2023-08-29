eb6907395019cb926a8d9d80ba0811feb900e60d
ChrolloRifat <officialmhrifat@outlook.com>
2023-08-29 21:16:44 +0600

    Add Course class

---
+++ src/CourseClass.java
@@ -0,0 +1,51 @@
+
+public class CourseClass {
+
+	public static void main(String[] args) {
+		Course course = new Course("CSE215");
+		System.out.println("Course: " + course.getName());
+		course.addStudent("Rifat");
+		course.addStudent("Ibrahimovic");
+		course.addStudent("Melanie");
+		System.out.println("Total Students: " + course.getNumberOfStudents());
+		for(int i = 0; i < course.getStudents().length; i++) {
+			System.out.println(course.getStudents()[i]);
+		}
+		
+
+	}
+
+}
+
+class Course{
+	private String name;
+	private String[] students;
+	private int numberOfStudents;
+	
+	Course(String name){
+		this.name = name;
+		students = new String[40];
+		numberOfStudents = 0;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void addStudent(String student) {
+		students[numberOfStudents] = student;
+		numberOfStudents++;
+	}
+	
+	public String[] getStudents() {
+		String[] studentList = new String[numberOfStudents];
+		for(int i = 0; i < studentList.length; i++) {
+			studentList[i] = students[i];
+		}
+		return studentList;
+	}
+	
+	public int getNumberOfStudents() {
+		return numberOfStudents;
+	}
+}
